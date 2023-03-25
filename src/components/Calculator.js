9f9e1fea738da9a76ac641c911085aaf6ea15115
getssh <gtamrat33@gmail.com>
2023-03-25 03:35:35 +0300

    Refactor Calculator function

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,54 +1,77 @@
-import {useState} from 'react'
+import { useState } from 'react';
 import './Calculator.css';
 import PropTypes from 'prop-types';
 import calculate from './logic/calculate';
 
-const [calculate, setCalculate] = useState({
-    total: null,
-    next: null,
-    operation: null,
-  });
-
-const CalcInside = (props) => {
-  const { calcText, colorClass } = props;
+const ShowResult = (props) => {
+  const { output } = props;
 
   return (
-    <div className={`wrap ${colorClass}`}>
-      <p>{calcText}</p>
+    <div className="shwo-result">
+      <p>
+        {output.total}
+        {output.operation}
+        {output.next}
+      </p>
     </div>
   );
 };
 
-CalcInside.propTypes = {
-  calcText: PropTypes.string.isRequired,
-  colorClass: PropTypes.string.isRequired,
+// const CalculatorOutput = ({ result }) => (
+//   <section className="CalcOutput">
+//     <span>
+//       {result.total}
+//       {result.operation}
+//       {result.next}
+//     </span>
+//   </section>
+// );
+
+ShowResult.propTypes = {
+  output: PropTypes.shape({
+    total: PropTypes.string,
+    next: PropTypes.string,
+    operation: PropTypes.string,
+  }).isRequired,
 };
 
-const Calculator = () => (
-  <div className="calculator">
-    <input type="text" placeholder="0" />
-    <div className="calc-content">
-      <CalcInside calcText="AC" colorClass="gray" />
-      <CalcInside calcText="+/-" colorClass="gray" />
-      <CalcInside calcText="%" colorClass="gray" />
-      <CalcInside calcText="÷" colorClass="orange" />
-      <CalcInside calcText="7" colorClass="gray" />
-      <CalcInside calcText="8" colorClass="gray" />
-      <CalcInside calcText="9" colorClass="gray" />
-      <CalcInside calcText="x" colorClass="orange" />
-      <CalcInside calcText="4" colorClass="gray" />
-      <CalcInside calcText="5" colorClass="gray" />
-      <CalcInside calcText="6" colorClass="gray" />
-      <CalcInside calcText="-" colorClass="orange" />
-      <CalcInside calcText="1" colorClass="gray" />
-      <CalcInside calcText="2" colorClass="gray" />
-      <CalcInside calcText="3" colorClass="gray" />
-      <CalcInside calcText="+" colorClass="orange" />
-      <CalcInside calcText="0" colorClass="gray wide" />
-      <CalcInside calcText="." colorClass="gray" />
-      <CalcInside calcText="=" colorClass="orange" />
+const handleChange = (e, calcState, calcSetState) => {
+  const calclated = calculate(calcState, e.target.textContent);
+  calcSetState({ ...calclated });
+};
+
+const Calculator = () => {
+  const [calculateResult, setCalculateResult] = useState({
+    total: '0',
+    next: null,
+    operation: null,
+  });
+
+  return (
+    <div className="calculator">
+      <ShowResult output={calculateResult} />
+      <div className="calc-content">
+        <button type="button" className="wrap" onClick={(e) => handleChange(e, calculateResult, setCalculateResult)}>AC</button>
+        <button type="button" className="wrap" onClick={(e) => handleChange(e, calculateResult, setCalculateResult)}>+/-</button>
+        <button type="button" className="wrap" onClick={(e) => handleChange(e, calculateResult, setCalculateResult)}>%</button>
+        <button type="button" className="wrap" onClick={(e) => handleChange(e, calculateResult, setCalculateResult)}>÷</button>
+        <button type="button" className="wrap" onClick={(e) => handleChange(e, calculateResult, setCalculateResult)}>7</button>
+        <button type="button" className="wrap" onClick={(e) => handleChange(e, calculateResult, setCalculateResult)}>8</button>
+        <button type="button" className="wrap" onClick={(e) => handleChange(e, calculateResult, setCalculateResult)}>9</button>
+        <button type="button" className="wrap" onClick={(e) => handleChange(e, calculateResult, setCalculateResult)}>x</button>
+        <button type="button" className="wrap" onClick={(e) => handleChange(e, calculateResult, setCalculateResult)}>4</button>
+        <button type="button" className="wrap" onClick={(e) => handleChange(e, calculateResult, setCalculateResult)}>5</button>
+        <button type="button" className="wrap" onClick={(e) => handleChange(e, calculateResult, setCalculateResult)}>6</button>
+        <button type="button" className="wrap" onClick={(e) => handleChange(e, calculateResult, setCalculateResult)}>-</button>
+        <button type="button" className="wrap" onClick={(e) => handleChange(e, calculateResult, setCalculateResult)}>1</button>
+        <button type="button" className="wrap" onClick={(e) => handleChange(e, calculateResult, setCalculateResult)}>2</button>
+        <button type="button" className="wrap" onClick={(e) => handleChange(e, calculateResult, setCalculateResult)}>3</button>
+        <button type="button" className="wrap" onClick={(e) => handleChange(e, calculateResult, setCalculateResult)}>+</button>
+        <button type="button" className="wrap" onClick={(e) => handleChange(e, calculateResult, setCalculateResult)}>0</button>
+        <button type="button" className="wrap" onClick={(e) => handleChange(e, calculateResult, setCalculateResult)}>=</button>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default Calculator;
