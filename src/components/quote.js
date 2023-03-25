ae0a88c41d87a8fb77dc06b351af591e6165ff48
getssh <gtamrat33@gmail.com>
2023-03-25 21:42:40 +0300

    Add quote component

---
+++ src/components/quote.js
@@ -0,0 +1,11 @@
+import React, { useState, useEffect } from 'react';
+
+const RandQuote = () => {
+  const [quote, setQuote] = useState('');
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState('');
+
+
+}
+
+export default RandQuote;
