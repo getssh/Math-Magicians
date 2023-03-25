e46382062055749bf59ff0093c0156171ce9e966
getssh <gtamrat33@gmail.com>
2023-03-25 21:55:13 +0300

    Add return value

--- src/components/quote.js
+++ src/components/quote.js
@@ -1,9 +1,9 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 
 const RandQuote = () => {
   const [quote, setQuote] = useState('');
   const [isLoading, setIsLoading] = useState(true);
-  const [error, setError] = useState('');
+  const [error] = useState('');
 
   useEffect(() => {
     fetch('https://api.api-ninjas.com/v1/quotes')
@@ -11,8 +11,10 @@ const RandQuote = () => {
       .then((data) => {
         setQuote(data.quote);
         setIsLoading(false);
-      })
-    }, []);
-}
+      });
+  }, []);
+
+  return <div>{quote}</div>;
+};
 
 export default RandQuote;
