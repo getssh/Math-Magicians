1bfb818de355cf9e1dca014884fbd31af1a293f9
getssh <gtamrat33@gmail.com>
2023-03-30 11:34:17 +0300

    Add react router and routes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Calculator from './components/Calculator';
 import RandQuote from './components/quote/quote';
 import Header from './components/header/Header';
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <Header />
-      <Calculator />
-      <RandQuote />
-    </>
+      <Routes>
+        <Route index element={<Home />} />
+        <Route path="Calculator" element={<Calculator />} />
+        <Route path="RandQuote" element={<RandQuote />} />
+        <Route path="*" element={<h3>Page not found</h3>} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
