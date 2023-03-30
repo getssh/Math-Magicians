4890ce4d11fba5cf52b6305b69749125b6ee6e9e
getssh <gtamrat33@gmail.com>
2023-03-30 11:08:35 +0300

    Add header function

---
+++ src/components/header/Header.js
@@ -0,0 +1,16 @@
+import './Header.css';
+
+const Header = () => (
+  <div className="header">
+    <h2>Math Magicians</h2>
+    <nav>
+      <ul>
+        <li>Home</li>
+        <li>Calculator</li>
+        <li>Quote</li>
+      </ul>
+    </nav>
+  </div>
+);
+
+export default Header;
