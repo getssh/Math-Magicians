134c1e6b27889d85d4200b99e0a745bb4b8fafdb
getssh <gtamrat33@gmail.com>
2023-03-30 11:10:22 +0300

    Add header by importing to app.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Calculator from './components/Calculator';
 import RandQuote from './components/quote/quote';
+import Header from './components/header/Header';
 
 function App() {
   return (
     <>
+      <Header />
       <Calculator />
       <RandQuote />
     </>
